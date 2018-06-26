8e85874420dfd1e34c0697e4ab9522c02bf3c3c8
Amanda Richardson <amanda162000@gmail.com>
2018-06-26 16:50:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,3 @@
-require 'rspec'
-
-def fizzBuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
-  else
-    number
-  end
+def fizzbuzz
 end
+
